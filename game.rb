334ce3cf82b404f3c71435995db30c9661e0d2f1
Wesley974 <milo974@gmail.com>
2016-10-31 21:23:39 +0400

    review

--- game.rb
+++ game.rb
@@ -51,20 +51,20 @@ class Game
   end
 
   def control
-    big_winner if turn.to_i == @num && @try == 1
-    winner if turn.to_i == @num
-    puts "Trop petit!" if turn.to_i < @num
-    puts "Top grand!" if turn.to_i > @num
+    big_winner if @turn.to_i == @num && @try == 1
+    winner if @turn.to_i == @num
+    puts "Trop petit!" if @turn.to_i < @num
+    puts "Top grand!" if @turn.to_i > @num
   end
 
   def run
-    turn = nil
+    @turn = nil
     @try = 0
     @start = Time.now
-    while turn != @num
+    while @turn != @num
       print "Essayer ? "
-      turn = gets.chomp
-      if turn == "quit" then
+      @turn = gets.chomp
+      if @turn == "quit" then
         exit
         elsif is_a_number?(turn) == false then
           puts "Oh, entre un nombre !!!"
