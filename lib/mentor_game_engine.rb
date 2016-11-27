8ba6ec4558ebaa1ddd065c8b90138399aeee0992
Wesley974 <milo974@gmail.com>
2016-11-27 20:17:00 +0400

    mentoring

--- lib/mentor_game_engine.rb
+++ lib/mentor_game_engine.rb
@@ -5,16 +5,29 @@ class GameEngine
   def initialize
     @secret = 1 + rand(MAXNUM)
     @attempts = 0
+    @start_time = Time.new
+  end
+
+  def a_number?(number)
+    number.to_s == number.to_i.to_s
   end
 
   def guess(number)
-    @attempts += 1
 
     result = 'too big' if number.to_i > @secret
     result = 'too small' if number.to_i < @secret
     result = 'a winner' if number.to_i == @secret
-
+    result = 'not a number' unless a_number?(number)
+    @attempts += 1 if result != 'not a number'
     result
+  end
+
+  def attempts
+    @attempts
+  end
+
+  def time
+    (Time.now - @start_time).round
   end
 end
 
