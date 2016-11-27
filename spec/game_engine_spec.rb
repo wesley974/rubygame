1a69bccb06b0dd300483778aa55e128767561290
Wesley974 <milo974@gmail.com>
2016-11-27 15:57:42 +0400

    expected

--- spec/game_engine_spec.rb
+++ spec/game_engine_spec.rb
@@ -1,21 +1,41 @@
 Rspec.describe GameEngine do
   describe "#initialize" do
+    it "initialiaze a max number" # maxnum
     it "initialize secret number" # secret
     it "initialize tries number" # tries
+    expect(maxnum).not_to be_empty
+    expect(tries).to be_empty
   end
 
   describe "#a_number?" do
     it "test if it is an integer"
+    expect(answer).to be_an(integer)
   end
 
   describe "#check" do
-    it "test if you wish to quit the game" # == 'quit'
+    context "test if you wish to quit the game" do
+      expect(answer).to eq('quit')
+    end
+
     it "test if your number is a integer" # a_number(answer)
     it "increase" # tries + 1
-    it "test if your number is <" # too smaller
-    it "test if your number is >" # too bigger
-    it "test if your number is == && if your tries is 1" # the magian winner
-    it "test if your number is ==" # the winner
+
+    context "too smaller" do
+      expect(answer).to be < secret
+    end
+
+    context "too bigger" do
+      expect(answer).to be > secret
+    end
+
+    context "a Magician" do
+     expect(answer).to eq(secret)
+     expect(tries).to eq(1)
+    do
+
+    context "a winner" do
+      expect(answer).to eq(secret)
+    end
   end
 
 end
