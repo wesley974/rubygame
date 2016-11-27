13721f8e4e117cb24bc5ce71f6845e8290bf3500
Wesley974 <milo974@gmail.com>
2016-11-27 16:08:14 +0400

    fix rspec

--- spec/game_engine_spec.rb
+++ spec/game_engine_spec.rb
@@ -1,24 +1,18 @@
 Rspec.describe GameEngine do
   describe "#initialize" do
-    context "initialiaze a max number" do
-      expect(maxnum).not_to be_empty # maxnum
-    end
-
+    it "initialiaze a max number" # maxnum
     it "initialize secret number" # secret
-
-    context "initialize tries number" do
-      expect(tries).to be_empty # tries
-    end
-
+    it "initialize tries number" # tries
   end
 
   describe "#a_number?" do
-    it "test if it is an integer"
-    expect(answer).to be_an(integer)
+    it "test if it is an integer" do
+      expect(answer).to be_an(integer)
+    end
   end
 
   describe "#check" do
-    context "test if you wish to quit the game" do
+    context "quit the game" do
       expect(answer).to eq('quit')
     end
 
