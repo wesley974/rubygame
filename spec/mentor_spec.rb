a5884f97caab231c97459ab37d389ae6e456df29
Wesley974 <milo974@gmail.com>
2016-11-27 17:42:29 +0400

    mentoring

---
+++ spec/mentor_spec.rb
@@ -0,0 +1,31 @@
+require_relative '../lib/game_engine' # here is class we are testing
+
+describe GameEngine do
+  subject { GameEngine.new }
+
+  describe '#initialize' do
+    it 'sets the number' do
+      expect(subject.number).to be > 0
+      expect(subject.number).to be < 100
+    end
+  end
+
+  describe '#guess' do
+    it 'increases number of attempts' do
+       subject.guess(5)
+       expect(subject.attempts).to eq 1
+       subject.guess(8)
+       expect(subject.attempts).to eq 2
+    end
+
+    context 'result' do
+      let(:number) { subject.number }
+
+      it 'returns result depending on guess' do
+        expect(subject.guess(number - 1)).to eq :bigger
+        expect(subject.guess(number + 1)).to eq :smaller
+        expect(subject.guess(number)).to eq :guess
+      end
+    end
+  end
+end
