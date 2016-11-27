d0023a14623c6b93224732986923f420bf64e393
Wesley974 <milo974@gmail.com>
2016-11-27 20:42:16 +0400

    mentoring

--- lib/mentor_game_engine.rb
+++ lib/mentor_game_engine.rb
@@ -15,8 +15,7 @@ class GameEngine
     result = 'too big' if number.to_i > @secret
     result = 'too small' if number.to_i < @secret
     result = 'a winner' if number.to_i == @secret
-    result = 'not a number' unless a_number?(number)
-    @attempts += 1 if result != 'not a number'
+    a_number?(number) ? @attempts += 1 : result = 'not a number'
     result
   end
 
@@ -29,7 +28,10 @@ class GameEngine
   end
 end
 
+# irb
+# require_relative 'mentor_game_engine'
 # player = GameEngine.new => start the engine
 # player.guess(50) => guess
+# player.guess('test') => 'not a number'
 # player.tries => get the attempts number
 # player.time => get time left in seconds
