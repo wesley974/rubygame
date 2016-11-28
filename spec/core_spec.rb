01482c980194cc9f4b9eeb0697435aea476975e7
Wesley <milo974@gmail.com>
2016-11-28 16:42:44 +0400

    improve code

--- spec/core_spec.rb
+++ spec/core_spec.rb
@@ -5,26 +5,26 @@ describe Core do
 
   describe '#initialize' do
     it 'sets the secret number' do
-      expect(subject.secret).to be > 0
-      expect(subject.secret).to be < 100
+      expect(@secret).to be > 0
+      expect(@secret).to be < 100
     end
   end
 
   describe '#guess' do
-    it 'increases number of attempts' do
+    it 'increases secret of attempts' do
        subject.guess(5)
-       expect(subject.attempts).to eq 1
+       expect(subject.tries).to eq 1
        subject.guess(8)
-       expect(subject.attempts).to eq 2
+       expect(subject.tries).to eq 2
     end
 
     context 'result' do
-      let(:number) { subject.number }
+      let(:number) { @secret }
 
       it 'returns result depending on guess' do
-        expect(subject.guess(number - 1)).to eq :bigger
-        expect(subject.guess(number + 1)).to eq :smaller
-        expect(subject.guess(number)).to eq :guess
+        expect(subject.guess(@secret - 1)).to eq :bigger
+        expect(subject.guess(@secret + 1)).to eq :smaller
+        expect(subject.guess(@secret)).to eq :guess
       end
     end
   end
