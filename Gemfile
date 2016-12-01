94f24fbdeefac2fc27b7ba03623bf70ef88c04c5
Wesley <milo974@gmail.com>
2016-12-01 14:56:31 +0400

    improve code

--- Gemfile
+++ Gemfile
@@ -1,5 +1,5 @@
 # frozen_string_literal: true
-source "https://rubygems.org"
+source 'https://rubygems.org'
 
-gem 'rubocop'
 gem 'rspec'
+gem 'rubocop'
--- lib/scoreboard.rb
+++ lib/scoreboard.rb
@@ -2,4 +2,19 @@ require 'fileutils'
 
 # Manage score file
 class Scoreboard
+  def initialize; end
+
+  def check_file; end
+
+  def self.view
+  end
+
+  def self.add
+  end
+
+  def self.number
+  end
+
+  def self.replace
+  end
 end
