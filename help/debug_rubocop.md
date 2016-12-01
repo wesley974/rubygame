125f3776b6987dc45c8afa0f441ea0735ce4060d
Wesley <milo974@gmail.com>
2016-12-01 11:25:42 +0400

    improve code

---
+++ help/debug_rubocop.md
@@ -0,0 +1,7 @@
+rubocop # analyse all your project
+rubocop file.rb # analyse just file.rb
+
+Options
+
+-a | --auto-correct # apply correction, backup the files before !!
+-E | --extra-details # give you more details
--- spec/core_spec.rb
+++ spec/core_spec.rb
@@ -5,12 +5,11 @@ describe Core do
     @core = Core.new
   end
 
-  it "should have default value tries as 0" do
+  it 'should have default value tries as 0' do
     expect(@core.instance_variable_get(:@tries)).to eql(0)
-
   end
 
-  it "should have secret range in between 1 and 100" do
+  it 'should have secret range in between 1 and 100' do
     expect(@core.instance_variable_get(:@secret)).to be_between(1, 100)
   end
 end
