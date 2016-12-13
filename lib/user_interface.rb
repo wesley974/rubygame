f551d5d109d1e004a0f726b93cb132a849213de8
Wesley <milo974@gmail.com>
2016-12-13 11:19:16 +0400

    improve code

--- lib/user_interface.rb
+++ lib/user_interface.rb
@@ -83,7 +83,7 @@ class UserInterface
   end
 
   def check_winner
-    puts " In #{@player.tries} attempts and in #{@player.time} seconds!\n"
+    puts "In #{@player.tries} attempts and in #{@player.time} seconds!\n"
     @player.tries == 1 ? big_winner : winner
     puts 'New Score !!'.red.bold \
       if @board.add(@name, @player.tries, @player.time)
