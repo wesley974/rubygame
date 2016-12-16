07efcc9a6a8d42663ac1d4ab6b46ceabf9b5cea9
Wesley <milo974@gmail.com>
2016-12-16 21:32:31 +0400

    private method

--- lib/scoreboard.rb
+++ lib/scoreboard.rb
@@ -19,17 +19,17 @@ class Scoreboard
     write if @score.any? { |row| row.timestamp == timestamp }
   end
 
-  def write
-    File.open(FLPATH, 'w') { |f| f.write(@score.to_yaml) }
-  end
-
   def info
     @score.map { |a| [a[:name], a[:tries], a[:time]] }
   end
+
+  private
+    def write
+      File.open(FLPATH, 'w') { |f| f.write(@score.to_yaml) }
+    end
 end
 
 # require_relative 'lib/scoreboard'
 # file = Scoreboard.new
 # file.add('Wesley',2,65) => number
 # file.info => [[], [], []]
-# file.write
