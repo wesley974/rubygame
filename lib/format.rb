7c6592c39ceee4f26cd3b35b253c0a0e38ca0c12
Wesley <milo974@gmail.com>
2017-03-14 14:53:55 +0400

    improve code

--- lib/format.rb
+++ lib/format.rb
@@ -2,16 +2,16 @@
 module Format
   SPACES = 50
   HEADER = %w(Name Attempts Time!).freeze
+  TITLE = 'The best 3 SCORE'
   def self.text(title: nil, msg: nil)
     puts "\n\n#{title.center(SPACES).green.bold}" if title
     puts "#{msg.center(SPACES)}\n" if msg
   end
 
   def self.table(datas)
-    table = Terminal::Table.new headings: HEADER, rows: datas
+    table = Terminal::Table.new title: TITLE, headings: HEADER, rows: datas
     table.align_column(1, :center)
     table.align_column(2, :center)
-    table.title = 'The best 3 SCORE'
     puts table.render.split("\n").map { |ln| ln.center(SPACES) }.join("\n")
   end
 end
