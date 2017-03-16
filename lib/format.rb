c0bbc83b0876aeb23e2f6ea330f5d646d770a44f
Wesley <milo974@gmail.com>
2017-03-16 09:54:52 +0400

    refactoring

--- lib/format.rb
+++ lib/format.rb
@@ -13,9 +13,13 @@ module Format
     t = Terminal::Table.new(title: TITLE, headings: HEADER, rows: datas)
     1.upto(2) { |x| t.align_column(x, :center) }
     puts t.render.split("\n").map { |ln|
-      ln = ln.center(SPACES).green.bold \
-      if winner && ln.include?(winner[0].to_s) && ln.include?(winner[1].to_s)
-      ln.center(SPACES)
+      Format.table_line(ln, winner)
     }.join("\n")
+  end
+
+  def self.table_line(ln, winner)
+    ln = ln.center(SPACES).green.bold \
+      if winner && ln.include?(winner[0].to_s) && ln.include?(winner[1].to_s)
+    ln.center(SPACES)
   end
 end
